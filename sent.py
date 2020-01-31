d154d1a243b638ddb8eb944624dccd09884172f5
Subrata Kumar Biswas <43948261+subrata3112@users.noreply.github.com>
2020-01-31 22:37:58 +0530

    Added output based on the sum of polarity.

--- sent.py
+++ sent.py
@@ -6,7 +6,7 @@ sentences = ["It is very sad evening.",
 "Its such a beautiful morning!",
 "I hate my life. There is no point in living anymore.",
 "Its hard to defend ourselves.",
-"Its impossible to defeat the guy.",]
+"Its impossible to defeat the guy."]
 
 sid = SentimentIntensityAnalyzer()
 
@@ -16,8 +16,13 @@ for x in sentences:
         sentences.extend(lines_list)
         sentences.remove(x)
 
+positivity = 0
 for x in sentences:
     ss = sid.polarity_scores(x)
 #    print(x)
 #    print(type(ss))
-    print(ss['pos'])
+#    print(ss['pos'])
+    positivity+=ss['pos']
+
+if positivity <= 3.1:
+    print("You seem to be depressed. Have a consultation!")
